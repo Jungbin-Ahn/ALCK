328221fee8c4ff4958fbb0de0e2d9bbc894935db
Jungbin-Ahn <65687288+Jungbin-Ahn@users.noreply.github.com>
2022-10-07 19:43:57 -0400

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -2,17 +2,35 @@ const time = document.querySelector("#timer span");
 const timerStartButton = document.querySelector("#timer button");
 const timerResetButton = document.querySelector("#timer :last-child");
 
-let sec = '15s';
+let TIME = 15;
+let timer;
 
-function showAlert(){
-    alert("시간초과!");
+
+function showAlert() {
+  alert("시간초과!");
 }
 
+function startTimer(){
+    updateTimer();
+    stopTimer();
+    timer = setInterval(updateTimer, 1500);
+}
+function stopTimer(){
+    clearInterval(timer);
+}
+function updateTimer(){
+    time.innerText = `${TIME}s`;
+    TIME--;
+    if(TIME < 0){
+        stopTimer();
+        TIME=15;
+        showAlert();
+    }
+}
 function resetTimer(){
-    alert("reset");
+    stopTimer();
+    TIME = 15;
+    time.innerText = `${TIME}s`;
 }
-
-
-
-timerStartButton.addEventListener("click",showAlert);
+timerStartButton.addEventListener("click", startTimer);
 timerResetButton.addEventListener("click", resetTimer);
